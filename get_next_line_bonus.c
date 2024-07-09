246021a1c597a84097f7acf94abc016166a63951
Israel Izquierdo Garcia <iizquier@c3r10s6.42malaga.com>
2024-07-09 16:24:57 +0200

    createextractline&upstack

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: iizquier <iizquier@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 15:31:25 by iizquier          #+#    #+#             */
-/*   Updated: 2024/07/03 19:14:46 by iizquier         ###   ########.fr       */
+/*   Updated: 2024/07/09 16:21:04 by iizquier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,59 @@
 
 char	*extract_line(char *stack)
 {
+		char	*line;
+	int 	i;
+	
+	if(!stack || !*stack)
+		return (NULL);
+	i = 0;
+	while (stack[i] != '\n' && stack[i] != '\0')
+		i++;
+	if (stack[i] == '\n')
+		i++;
+	line = malloc(sizeof(char) * (i + 1));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (stack[i] != '\n' && stack[i] != '\0')
+	{
+		line[i] = stack[i];
+		i++;
+	}
+	if (stack[i] == '\n')
+	{
+		line[i++] = '\n';
+	}
+	line[i] = '\0';
+	return (line);
 }
 
 char	*up_stack(char *stack)
 {
+	char *tmp;
+	char *ptr;
+	int i;
+	
+	ptr = ft_strchr(stack, '\n');
+	if (!ptr)
+	{
+		free(stack);
+		return (NULL);
+	}
+	ptr++;
+	tmp = malloc(sizeof(char) * (ft_strlen(ptr) + 1));
+	if (!tmp)
+		return (NULL);
+	i = 0;
+	while (ptr != '\0')
+	{
+		tmp[i] = *ptr;
+		i++;
+		ptr++;
+	}
+	tmp[i] = '\0';
+	free(stack);
+	return (tmp);
 }
 
 char	*joinfree(char *stack, char *tmp)
